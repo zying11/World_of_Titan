a30f5836427a3394ed33bcfaecf43b9523119cbe
zy <yingying22226@gmail.com>
2022-05-29 15:45:18 +0800

    Rename the file for controller and fxml
    - prompt-user-enter-numTitan-window -> prompt-user-enter-numTitan-page
    
    - PromptUserEnterNumTitanWindowController -> PromptUserEnterNumTitanPageController

--- src/main/java/org/example/PromptUserEnterNumTitanPageController.java
+++ src/main/java/org/example/PromptUserEnterNumTitanPageController.java
@@ -2,6 +2,8 @@ package org.example;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -9,8 +11,9 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
-public class PromptUserEnterNumTitanWindowController {
+public class PromptUserEnterNumTitanPageController {
     /**
      * Stage is used to represent a window in a JavaFX desktop application
      */
@@ -25,6 +28,8 @@ public class PromptUserEnterNumTitanWindowController {
      * Root provides a solution to the issue of defining a reusable component with FXML
      */
     private Parent root;
+
+    static String input;
 
     @FXML
     private TextField numOfTitanGenerate;
@@ -41,6 +46,27 @@ public class PromptUserEnterNumTitanWindowController {
             inputError=true;
             System.out.println("Empty input");
             errorLabel.setText("Empty field!!");
+        }
+
+        else if(Character.isDigit(numOfTitanGenerate.getText().charAt(0))){
+            input=numOfTitanGenerate.getText();
+            System.out.println("Generating titan");
+        }
+
+        else{
+            inputError=true;
+            System.out.println("Please enter number");
+            errorLabel.setText("Please enter number");
+        }
+
+        //when no error, go to generate titan loading page
+        if(!inputError){
+            //forward to generate titan loading page
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("generate-titan-loading-page.fxml")));
+            stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
         }
     }
 }
